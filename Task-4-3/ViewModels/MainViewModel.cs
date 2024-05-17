a60d03a0f3b9db57388fe4b552bb7700e9c5f19c
miru <shchk1002@gmail.com>
2024-05-17 19:31:04 +0300

    Добавьте файлы проекта.

---
+++ Task-4-3/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Task_4_3.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Text => "The Sukhoi Su-57 is a twin-engine stealth multirole fighter aircraft developed by Sukhoi. It is the product of the PAK FA programme, which was initiated in 1999 as a more modern and affordable alternative to the MFI. Sukhoi's internal designation for the aircraft is T-50. The Su-57 is the first aircraft in Russian military service designed with stealth technology and is intended to be the basis for a family of stealth combat aircraft. A multirole fighter capable of aerial combat as well as ground and maritime strike, the Su-57 incorporates stealth, supermaneuverability, supercruise, integrated avionics and large payload capacity. The aircraft is expected to succeed the MiG-29 and Su-27 in the Russian military service and has also been marketed for export.\r\n\r\n";
+}
---
+++ Task-4-3/Views/MainView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Task_4_3.ViewModels;
+
+namespace Task_4_3.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
